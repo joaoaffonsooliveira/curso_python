5e95e51d55be1aae92e1f34f06bde6e5322ec5c5
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2021-05-21 11:17:26 -0300

    Adiciona primeirosComandos.py

---
+++ primeirosPassos/primeirosComandos.py
@@ -0,0 +1,19 @@
+print('Olá mundo!')
+print(7 + 4)
+print('7' + '4') # concatenação --> 74
+#print('Olá' + 5) # error!
+print('Olá', 5) # concatenou olá 5
+
+# toda variável é um objeto para o python
+
+nome = 'Guanabara'
+idade = 25
+peso = 74.8
+print(nome, idade, peso)
+#print(nome + idade + peso) # error!
+
+nome = input('Qual é o seu nome?')
+idade = input('Qual é a sua idade?')
+peso = input('Qual é o seu peso?')
+
+print(nome, idade, peso)
